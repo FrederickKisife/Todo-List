0dedf0d81b37f5254d8ee2deb0bdee2858d66686
FrederickKisife <kilafrederick@gmaul.com>
2023-02-20 11:44:52 -0800

    finished setting up the JS

--- app.js
+++ app.js
@@ -32,16 +32,16 @@ ul.addEventListener("click", e=>{
 
 const filterTodos = (term)=>{
     Array.from(ul.children)
-    .filter((todo)=> !todo.textContent.includes(term))
+    .filter((todo)=> !todo.textContent.toLowerCase().includes(term))
     .forEach((todo)=>todo.classList.add('filtered'))
 
     Array.from(ul.children)
-    .filter((todo)=> !todo.textContent.includes(term))
+    .filter((todo)=> todo.textContent.toLowerCase().includes(term))
     .forEach((todo)=>todo.classList.remove('filtered'))
 }
 
 //keyup event
 search.addEventListener("keyup", ()=>{
-    const term = search.value.trim();
+    const term = search.value.trim().toLowerCase();
     filterTodos(term)
 })
